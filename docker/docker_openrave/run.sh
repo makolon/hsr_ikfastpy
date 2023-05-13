b547fe3a24c99df896baa40e902402b4febef200
makolon <s.makolon@icloud.com>
2023-05-13 21:39:32 +0900

    modify docker

--- docker/docker_openrave/run.sh
+++ docker/docker_openrave/run.sh
@@ -1,4 +1,4 @@
 docker run -it --rm --net host --privileged \
-    -v `pwd`:/ikfast \
-    -v `pwd`/hsrb_ikfast:/root/.openrave \
+    -v ~/hsr_ikfastpy:/ikfast \
+    -v ~/hsr_ikfastpy/hsrb_ikfast:/root/.openrave \
     --name openrave_docker openrave-ikfast-docker:latest bash
